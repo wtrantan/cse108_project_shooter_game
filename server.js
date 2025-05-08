f8755e1850c0da596f78d3145a018f2726675dfe
William Trantan <wtrantan@gmail.com>
2025-05-08 14:07:48 -0700

    changed player speed

--- server.js
+++ server.js
@@ -1936,7 +1936,7 @@ function handlePlayerHit(playerId, bullet) {
 }
 setInterval(updateServerBullets, 33); // ~30 updates per second
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
